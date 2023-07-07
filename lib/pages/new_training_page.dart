254a15821fba63980a2ad07f335f3e9a877a3def
racalixta <ra.calixta@gmail.com>
2023-07-07 12:30:59 -0300

    melhorando a page newTraining

--- lib/pages/new_training_page.dart
+++ lib/pages/new_training_page.dart
@@ -11,6 +11,35 @@ class _NewTrainingPageState extends State<NewTrainingPage> {
   final formKey = GlobalKey<FormState>();
   final title = TextEditingController();
   final details = TextEditingController();
+  final twoPoints = TextEditingController();
+  final threePoints = TextEditingController();
+  bool cornerLeft = false;
+  bool wingLeft = false;
+  bool center = false;
+  bool wingRight = false;
+  bool cornerRight = false;
+
+  change(value, switchValue) {
+    setState(() {
+      switch (switchValue) {
+        case 'cornerLeft':
+          cornerLeft = value;
+          break;
+        case 'wingLeft':
+          wingLeft = value;
+          break;
+        case 'center':
+          center = value;
+          break;
+        case 'cornerRight':
+          cornerRight = value;
+          break;
+        case 'wingRight':
+          wingRight = value;
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +56,7 @@ class _NewTrainingPageState extends State<NewTrainingPage> {
       // backgroundColor: const Color.fromARGB(255, 241, 241, 241),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.only(top: 10),
+          padding: const EdgeInsets.only(top: 8),
           child: Form(
             key: formKey,
             child: Column(
@@ -48,6 +77,139 @@ class _NewTrainingPageState extends State<NewTrainingPage> {
                       }
                       return null;
                     },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: TextFormField(
+                    controller: details,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Detalhes',
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1,
+                    maxLines: 5,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Informe os detalhes sobre o treino!';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Dois Pontos',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      CheckboxListTile(
+                        value: cornerLeft,
+                        title: const Text('Zona Morta Esquerda'),
+                        onChanged: (value) => change(value, 'cornerLeft'),
+                      ),
+                      CheckboxListTile(
+                        value: wingLeft,
+                        title: const Text('Ala Esquerda'),
+                        onChanged: (value) => change(value, 'wingLeft'),
+                      ),
+                      CheckboxListTile(
+                        value: center,
+                        title: const Text('Centro'),
+                        onChanged: (value) => change(value, 'center'),
+                      ),
+                      CheckboxListTile(
+                        value: wingRight,
+                        title: const Text('Ala Direita'),
+                        onChanged: (value) => change(value, 'wingRight'),
+                      ),
+                      CheckboxListTile(
+                        value: cornerRight,
+                        title: const Text('Zona Morta Direita'),
+                        onChanged: (value) => change(value, 'cornerRight'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: TextFormField(
+                          controller: twoPoints,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Quantidade de Chutes',
+                          ),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe a quantidade corretamente!';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(),
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Três Pontos',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      CheckboxListTile(
+                        value: cornerLeft,
+                        title: const Text('Zona Morta Esquerda'),
+                        onChanged: (value) => change(value, 'cornerLeft'),
+                      ),
+                      CheckboxListTile(
+                        value: wingLeft,
+                        title: const Text('Ala Esquerda'),
+                        onChanged: (value) => change(value, 'wingLeft'),
+                      ),
+                      CheckboxListTile(
+                        value: center,
+                        title: const Text('Centro'),
+                        onChanged: (value) => change(value, 'center'),
+                      ),
+                      CheckboxListTile(
+                        value: wingRight,
+                        title: const Text('Ala Direita'),
+                        onChanged: (value) => change(value, 'wingRight'),
+                      ),
+                      CheckboxListTile(
+                        value: cornerRight,
+                        title: const Text('Zona Morta Direita'),
+                        onChanged: (value) => change(value, 'cornerRight'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8),
+                        child: TextFormField(
+                          controller: threePoints,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Quantidade de Chutes',
+                          ),
+                          keyboardType: TextInputType.number,
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Informe a quantidade corretamente!';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
                 Padding(
